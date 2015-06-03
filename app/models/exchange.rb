6fd1eeede5b24e8bc8adf78d2652888bb1db4c40
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-06-03 17:31:42 -0400

    Add current and previous month debits to html view

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -9,4 +9,33 @@ class Exchange < ActiveRecord::Base
   def self.current_month
     self.all.select {|e| e.created_at.month == Time.now.month}
   end
+
+  def self.previous_month
+    self.all.select {|e| e.created_at.month == Time.now.month-1}
+  end
+
+  def self.current_debits
+    total = 0
+    self.current_month.each {|e| total += e.debits.to_f}
+    total
+  end
+
+  def self.previous_debits
+    total = 0
+    self.previous_month.each {|e| total += e.debits.to_f}
+    total
+  end
+
+  def self.big_expense
+    expense = 0
+    self.current_month.each do |e|
+      if e.debits.to_f > expense
+        e.debits.to_f = expense
+      end
+    expense
+  end
+
+
+
+
 end
--- test/models/exchange_test.rb
+++ test/models/exchange_test.rb
@@ -2,13 +2,22 @@ require 'test_helper'
 
 class ExchangeTest < ActiveSupport::TestCase
   test "total balance" do
-    assert_equal 194.9, Exchange.balance
+    assert_equal 379.9, Exchange.balance
   end
 
   test "current month transcations" do
     assert_equal 4, Exchange.current_month.count
   end
 
+  test "previous month transcations" do
+    assert_equal 1, Exchange.previous_month.count
+  end
+
+  test "current month debits" do
+  ends
+
+  test "previous month debits" do
+  end
 
   # test "the truth" do
   #   assert true
